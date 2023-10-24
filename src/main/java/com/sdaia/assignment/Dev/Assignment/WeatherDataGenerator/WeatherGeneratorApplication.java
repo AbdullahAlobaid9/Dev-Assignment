7707f3399210d1828af4a8c504e33729e3eacb44
Abdullah Fahad <a.fahad9@outlook.com>
2023-10-24 19:42:59 +0300

    Configured the main application class for Spring Boot

--- src/main/java/com/sdaia/assignment/Dev/Assignment/WeatherDataGenerator/WeatherGeneratorApplication.java
+++ src/main/java/com/sdaia/assignment/Dev/Assignment/WeatherDataGenerator/WeatherGeneratorApplication.java
@@ -1,4 +1,12 @@
 package com.sdaia.assignment.Dev.Assignment.WeatherDataGenerator;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class WeatherGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherGeneratorApplication.class, args);
+    }
 }
