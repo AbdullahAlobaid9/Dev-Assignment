84e79a675d1db413b6c9d5bb8f7d2bc195bb3d3a
Abdullah Fahad <a.fahad9@outlook.com>
2023-10-25 21:49:39 +0300

    Configured the main application class for Spring Boot

--- src/main/java/com/sdaia/assignment/Dev/Assignment/MonitorApp/MonitorAppApplication.java
+++ src/main/java/com/sdaia/assignment/Dev/Assignment/MonitorApp/MonitorAppApplication.java
@@ -1,4 +1,12 @@
 package com.sdaia.assignment.Dev.Assignment.MonitorApp;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class MonitorAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorAppApplication.class, args);
+    }
 }
