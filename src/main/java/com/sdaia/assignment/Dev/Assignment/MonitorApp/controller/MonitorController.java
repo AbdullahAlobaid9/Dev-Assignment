1ada16cf5c493c91e6f29b9455cf4ffca396aa10
Abdullah Fahad <a.fahad9@outlook.com>
2023-10-25 21:48:21 +0300

    implemented MonitorController class which handles incoming requests and Kafka data consumption

--- src/main/java/com/sdaia/assignment/Dev/Assignment/MonitorApp/controller/MonitorController.java
+++ src/main/java/com/sdaia/assignment/Dev/Assignment/MonitorApp/controller/MonitorController.java
@@ -1,4 +1,22 @@
 package com.sdaia.assignment.Dev.Assignment.MonitorApp.controller;
 
+import com.sdaia.assignment.Dev.Assignment.MonitorApp.model.WeatherData;
+import com.sdaia.assignment.Dev.Assignment.MonitorApp.repository.WeatherDataRepository;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class MonitorController {
+
+    private final WeatherDataRepository weatherDataRepository;
+
+    public MonitorController(WeatherDataRepository weatherDataRepository) {
+        this.weatherDataRepository = weatherDataRepository;
+    }
+
+    @KafkaListener(topics = "your-kafka-topic")
+    public void consumeWeatherData(WeatherData weatherData) {
+        // Store the received data in the database
+        weatherDataRepository.save(weatherData);
+    }
 }
