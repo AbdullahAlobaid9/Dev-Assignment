1c8c81864249b8475b4a78cd425ed43cc01d6560
Abdullah Fahad <a.fahad9@outlook.com>
2023-10-25 21:40:43 +0300

    Added IngestionController for ingesting weather data

--- src/main/java/com/sdaia/assignment/Dev/Assignment/IngestionService/controller/IngestionController.java
+++ src/main/java/com/sdaia/assignment/Dev/Assignment/IngestionService/controller/IngestionController.java
@@ -1,4 +1,22 @@
 package com.sdaia.assignment.Dev.Assignment.IngestionService.controller;
 
+import com.sdaia.assignment.Dev.Assignment.IngestionService.service.IngestionService;
+import com.sdaia.assignment.Dev.Assignment.WeatherDataGenerator.model.WeatherData;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class IngestionController {
+
+    private final IngestionService ingestionService;
+
+    public IngestionController(IngestionService ingestionService) {
+        this.ingestionService = ingestionService;
+    }
+
+    @PostMapping("/ingest-weather")
+    public void ingestWeatherData(@RequestBody WeatherData weatherData) {
+        ingestionService.publishToKafka(weatherData);
+    }
 }
