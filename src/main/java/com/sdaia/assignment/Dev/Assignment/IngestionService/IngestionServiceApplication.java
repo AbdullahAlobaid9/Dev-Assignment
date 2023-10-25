5cbc65f2d7acf4da3c67999160fbc265f96085f3
Abdullah Fahad <a.fahad9@outlook.com>
2023-10-25 21:42:52 +0300

    Configure Spring Boot application for the Ingestion Service

--- src/main/java/com/sdaia/assignment/Dev/Assignment/IngestionService/IngestionServiceApplication.java
+++ src/main/java/com/sdaia/assignment/Dev/Assignment/IngestionService/IngestionServiceApplication.java
@@ -1,4 +1,12 @@
 package com.sdaia.assignment.Dev.Assignment.IngestionService;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class IngestionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngestionServiceApplication.class, args);
+    }
 }
