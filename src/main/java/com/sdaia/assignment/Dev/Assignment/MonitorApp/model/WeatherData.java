ecd1437dc49bd60bdf7532268391108bbfe652b7
Abdullah Fahad <a.fahad9@outlook.com>
2023-10-25 21:45:26 +0300

    Added WeatherData entity class to represent weather data in DB

--- src/main/java/com/sdaia/assignment/Dev/Assignment/MonitorApp/model/WeatherData.java
+++ src/main/java/com/sdaia/assignment/Dev/Assignment/MonitorApp/model/WeatherData.java
@@ -1,4 +1,29 @@
 package com.sdaia.assignment.Dev.Assignment.MonitorApp.model;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "weather_data")
 public class WeatherData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "location_name")
+    private String locationName;
+
+    @Column(name = "latitude")
+    private double latitude;
+
+    @Column(name = "longitude")
+    private double longitude;
+
+    @Column(name = "time")
+    private LocalDateTime time;
+
+    @Column(name = "degree")
+    private double degree;
+
+    // Constructors, getters, and setters
 }
